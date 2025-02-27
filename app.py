3321b6cdb83599d652c5e3d0fb69da4d02fb0bbc
Dhaxina Kumar <73400073+dhaxina@users.noreply.github.com>
2025-02-28 01:27:17 +0530

    Update app.py
    -

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
+import uvicorn
 from fastapi import FastAPI
-import asyncio
-app=FastAPI()
-@app.get("/greetings")
-async def greetings():
-    await asyncio.sleep(10)
-    return {"message":"Hello World"}
+from pydantic import BaseModel
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+app = FastAPI()
+
+# Load Model
+try:
+    model = pickle.load("./model/spam_model.0.1.0.pkl")
+    print("Model Loaded Successfully!")
+except Exception as e:
+    print(f"Error Loading Model: {e}")
+
+# Input Schema
+class EmailInput(BaseModel):
+    text: str
+
+@app.get("/")
+def home():
+    return {"message": "Spam Classification API is Running!"}
+
+@app.post("/predict")
+def predict_spam(email: EmailInput):
+    prediction = model.predict([email.text])[0]
+    return {"spam": bool(prediction)}
+
+if __name__ == "__main__":
+    print("Running FastAPI server")
+    uvicorn.run(app, host="0.0.0.0", port=8001, reload=True)
